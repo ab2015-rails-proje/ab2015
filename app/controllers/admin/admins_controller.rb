e51495071d8b5da02d9630a71d35f954cbc73119
nolcay <olcay.oransoy@gmail.com>
2015-02-03 12:14:33 +0200

    close #39 admin kendisini bloklayıp aktif edemez

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -28,31 +28,35 @@ class Admin::AdminsController < Admin::BaseController
   end
 
   def block_all
-    Admin.update_all(blocked: true)
+    Admin.all.each {|a| a.update(blocked: true) unless current_admin == @admin }
     flash[:success] = 'İşlem Başarılı'
     redirect_to :back
   end
 
 	def block
     @admin = Admin.find(params[:id])
-    @admin.blocked = true
-    if @admin.save
-      flash[:success] = 'Başarılı bir şekilde bloklandı.'
-    else
-      flash[:danger] = 'Bloklanamadı!'
+    unless current_admin == @admin
+      @admin.blocked = true
+      if @admin.save
+        flash[:success] = 'Başarılı bir şekilde bloklandı.'
+      else
+        flash[:danger] = 'Bloklanamadı!'
+      end
+      redirect_to :back
     end
-    redirect_to :back
   end
 
 	def unblock
     @admin = Admin.find(params[:id])
-    @admin.blocked = false
-    if @admin.save
-      flash[:success] = 'Başarılı bir şekilde aktif edildi.'
-    else
-      flash[:danger] = 'Aktif edilemedi.'
+    unless current_admin == @admin
+      @admin.blocked = false
+      if @admin.save
+        flash[:success] = 'Başarılı bir şekilde aktif edildi.'
+      else
+        flash[:danger] = 'Aktif edilemedi.'
+      end
+     redirect_to :back  
     end
-    redirect_to :back
   end
 
 private
